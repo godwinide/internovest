8ad942296258b631f88661ad031ce18d111cab00
Godwin Idemudia <godwinide@gmail.com>
2023-12-29 16:29:31 +0100

    new

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(passport.session());
 
 // Global variables
 app.use(function (req, res, next) {
-  res.locals.sitename = "SwiftNeus"
+  res.locals.sitename = "SwiftNexus"
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
